bc7ab1a9f98d05695157037210ddebcfcdef976c
Scotty Mazyck <scotty.x.mazyck@AD.DICELAB.NET>
2020-03-02 20:15:58 +0000

    fixed PORT property

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var express = require('express')
     , app = express()
     , socketIO = require('socket.io')
 
-var port = process.env.port || 1337
+var port = process.env.PORT || 1337
 var server = app.listen(port, ()=> {
     console.log(`listening on port ${port}`)
 })
 
 app
 .get('/', (req, res)=>{
-    res.status(200).sendFile(__dirname + '\index.html')
+    res.status(200).sendFile(__dirname + '/index.html')
 })
 .get('/about', (req, res)=>{
     res.send(`
